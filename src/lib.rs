232e279b2c3c992ce84e81e09cdc0ecf54bf34cf
Kjell Kongsvik <kjell.kongsvik@gmail.com>
2019-10-31 23:00:01 +0100

    Posts to brewfather + refactorings (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,55 @@
 use byteorder::{BigEndian, ReadBytesExt};
+use serde::Serialize;
 use std::collections::HashMap;
-use std::convert::TryInto;
+use std::convert::{TryFrom, TryInto};
 use std::io::Cursor;
 use uuid::Uuid;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Clone)]
 pub struct Tilt {
-    pub uuid: Uuid,
-    pub t: f32,
-    pub sg: f32,
+    pub name: String,
+    pub gravity: f32,
+    pub temp: f32,
 }
 
-fn from(item: &Vec<u8>) -> Option<Tilt> {
-    let arr: [u8; 25] = (&item[..]).try_into().ok()?;
-    let u = Uuid::from_bytes(&arr[4..20]).ok()?;
-    let mut rdr = Cursor::new(&arr[20..24]);
-    let t: f32 = rdr.read_u16::<BigEndian>().ok()?.try_into().ok()?;
-    let sg: f32 = rdr.read_u16::<BigEndian>().ok()?.try_into().ok()?;
-
-    Some(Tilt {
-        uuid: u,
-        sg: sg / 1000.0,
-        t: (t - 32.0) / 1.8,
-    })
+#[derive(Debug)]
+pub enum TiltError {
+    NoTiltResponse,
+    Length,
+    Read,
+    Endian,
 }
 
-pub fn filter_tilts(data: &Vec<u8>, tilt_uuids: HashMap<Uuid, String>) -> Option<Tilt> {
-    let t = from(data)?;
-    match tilt_uuids.contains_key(&t.uuid) {
-        true => Some(t),
-        _ => None,
+impl TryFrom<&Vec<u8>> for Tilt {
+    type Error = TiltError;
+
+    fn try_from(v: &Vec<u8>) -> Result<Self, Self::Error> {
+        let arr: [u8; 25] = (&v[..]).try_into().map_err(|_| TiltError::Length)?;
+        let u = Uuid::from_bytes((&v[4..20]).try_into().map_err(|_| TiltError::Length)?);
+        let mut rdr = Cursor::new(&arr[20..24]);
+        let temp: f32 = rdr
+            .read_u16::<BigEndian>()
+            .map_err(|_| TiltError::Read)?
+            .try_into()
+            .map_err(|_| TiltError::Endian)?;
+        let gravity: f32 = rdr
+            .read_u16::<BigEndian>()
+            .map_err(|_| TiltError::Read)?
+            .try_into()
+            .map_err(|_| TiltError::Endian)?;
+        let uu = format!("{:?}", u);
+        Ok(Tilt {
+            name: uu,
+            gravity: gravity / 1000.0,
+            temp: (temp - 32.0) / 1.8,
+        })
     }
 }
 
-pub fn tilt_uuids() -> HashMap<Uuid, String> {
+pub fn tilt_uuids() -> HashMap<String, String> {
     let mut uuids = HashMap::new();
-    uuids.insert(
-        "A495BB80C5B14B44B5121370F02D74DE".parse().unwrap(),
-        "pink".to_string(),
-    );
+    let u: Uuid = "A495BB80C5B14B44B5121370F02D74DE".parse().unwrap();
+    uuids.insert("pink".to_string(), format!("{:?}", u));
     uuids
 }
 
@@ -47,17 +58,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_into_tilt() {
-        let pink_bytes: Vec<u8> = vec![
+    fn test_into_tilt() -> Result<(), TiltError> {
+        let pink_bytes: [u8; 25] = [
             76, 0, 2, 21, 164, 149, 187, 128, 197, 177, 75, 68, 181, 18, 19, 112, 240, 45, 116,
             222, 0, 67, 4, 4, 34,
         ];
-        let other_bytes: Vec<u8> = vec![
-            76, 0, 2, 21, 164, 159, 187, 128, 197, 177, 75, 68, 181, 18, 19, 112, 240, 45, 116,
-            222, 0, 67, 4, 4, 34,
-        ];
-
-        assert!(&filter_tilts(&pink_bytes, tilt_uuids()).is_some());
-        assert!(filter_tilts(&other_bytes, tilt_uuids()).is_none());
+        assert!(Tilt::try_from(&pink_bytes.to_vec()).is_ok());
+        assert!(Tilt::try_from(&pink_bytes[..=23].to_vec()).is_err());
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 extern crate rand;
 extern crate rumble;
+mod brewfather;
 mod lib;
 
-use lib::filter_tilts;
+use clap::{App, Arg};
 use lib::tilt_uuids;
-use lib::Tilt;
+use lib::{Tilt, TiltError};
 use rumble::api::{Central, Peripheral};
 use rumble::bluez::adapter::ConnectedAdapter;
 use rumble::bluez::manager::Manager;
+use serde_json::json;
+use std::convert::TryFrom;
 use std::thread;
 use std::time::Duration;
 
@@ -26,24 +29,56 @@ fn connect_adapter(dev: usize) -> Result<ConnectedAdapter, rumble::Error> {
     adapter.connect()
 }
 
-fn scan_tilt(adapter: &ConnectedAdapter) -> Vec<Tilt> {
-    for _ in 0..20 {
+fn scan_tilt(adapter: &ConnectedAdapter, timeout: i32) -> Result<Tilt, TiltError> {
+    let uuids = tilt_uuids();
+    for _ in 0..timeout {
         thread::sleep(Duration::from_secs(1));
         let tilts: Vec<Tilt> = adapter
             .peripherals()
             .into_iter()
             .filter_map(|p| p.properties().manufacturer_data)
-            .filter_map(|d| filter_tilts(&d, tilt_uuids()))
+            .filter_map(|data| Tilt::try_from(&data).ok())
+            .filter(|tilt| uuids.values().any(|u| u == &tilt.name))
             .collect();
-        if tilts.len() > 0 {
-            return tilts;
-        };
+        if let Some(t) = tilts.first() {
+            return Ok(t.clone());
+        }
     }
-    vec![]
+    Err(TiltError::NoTiltResponse)
 }
 
-pub fn main() {
+pub fn main() -> Result<(), TiltError> {
+    let args = App::new("Tilt logger")
+        .arg(
+            Arg::with_name("url")
+                .help("if set posts there")
+                .value_name("BREWFATHER_URL")
+                .required(false),
+        )
+        .arg(
+            Arg::with_name("timeout")
+                .short("t")
+                .long("timeout")
+                .help("Number of seconds to wait for a tilt")
+                .value_name("TIMEOUT_SECONDS")
+                .required(false),
+        )
+        .get_matches();
+
+    let url = args.value_of("url").unwrap_or("");
+    let timeout: i32 = args.value_of("timeout").unwrap_or("10").parse().unwrap();
+
     let adapter = connect_adapter(0).unwrap();
     adapter.start_scan().unwrap();
-    println!("{:?}", scan_tilt(&adapter));
+    let mut ts = scan_tilt(&adapter, timeout)?;
+    ts.name = "pink".to_string();
+    println!("{:?}", json!(&ts));
+
+    if !url.is_empty() {
+        match brewfather::post(url, &json!(&ts)) {
+            Ok(r) => println!("{:?}", r),
+            Err(e) => println!("{:?}", e),
+        };
+    }
+    Ok(())
 }
