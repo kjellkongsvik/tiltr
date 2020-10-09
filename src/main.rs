cd4f30514397f50c5d4736b6201156ffe552b489
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2020-10-09 14:44:39 +0200

    anyhow errors are simpler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use btleplug::api::{Central, Peripheral};
 use btleplug::bluez::adapter::ConnectedAdapter;
 use btleplug::bluez::manager::Manager;
@@ -5,20 +6,19 @@ use clap::{value_t, App, Arg};
 use std::convert::{TryFrom, TryInto};
 use std::thread;
 use std::time::Duration;
-
 mod tilt;
 use crate::tilt::Tilt;
 
-fn connect_adapter() -> Result<ConnectedAdapter, btleplug::Error> {
+fn connect_adapter() -> Result<ConnectedAdapter> {
     let manager = Manager::new()?;
 
     let adapter = manager
         .adapters()?
         .into_iter()
         .next()
-        .ok_or(btleplug::Error::DeviceNotFound)?;
+        .context("Device not found")?;
 
-    adapter.connect()
+    Ok(adapter.connect()?)
 }
 
 fn scan_tilt(adapter: &ConnectedAdapter, timeout: usize) -> Option<Tilt> {
@@ -38,21 +38,21 @@ fn scan_tilt(adapter: &ConnectedAdapter, timeout: usize) -> Option<Tilt> {
     None
 }
 
-fn main() -> Result<(), btleplug::Error> {
+fn main() -> Result<()> {
     let args = App::new("Tilt logger")
         .arg(Arg::with_name("calibrate_sg").short("c").default_value("0"))
         .arg(Arg::with_name("timeout").short("t").default_value("1"))
         .get_matches();
 
-    let timeout = value_t!(args.value_of("timeout"), usize).unwrap_or_else(|e| e.exit());
-    let calibrate = value_t!(args.value_of("calibrate_sg"), f32).unwrap_or_else(|e| e.exit());
+    let timeout = value_t!(args.value_of("timeout"), usize)?;
+    let calibrate = value_t!(args.value_of("calibrate_sg"), f32)?;
 
     let adapter = connect_adapter()?;
     adapter.start_scan()?;
 
     if let Some(mut t) = scan_tilt(&adapter, timeout) {
         t.gravity += calibrate;
-        println!("{}", serde_json::to_string(&t).unwrap());
+        println!("{}", serde_json::to_string(&t)?);
     }
 
     adapter.stop_scan()?;
