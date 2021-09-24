f3f9f54c316dfd95956cdd9e630ddcbb5c08b7d3
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2021-09-24 10:12:47 +0200

    wip

--- src/tilt.rs
+++ src/tilt.rs
@@ -1,6 +1,8 @@
-use serde::Serialize;
+use anyhow::Result;
+use bincode::Options;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::convert::{TryFrom, TryInto};
+use std::convert::TryFrom;
 use uuid::Uuid;
 
 #[derive(Serialize)]
@@ -11,7 +13,56 @@ pub struct Tilt {
 }
 
 #[derive(Debug)]
-pub struct NotATilt;
+pub enum TiltErr {
+    NotIbeacon,
+    ButDucky,
+    NotATilt,
+    BadValue,
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+struct RawTilt {
+    _t: u8,
+    _l: u8,
+    name: uuid::Bytes,
+    major: u16,
+    minor: u16,
+    _u: u8,
+}
+
+impl TryFrom<&HashMap<u16, Vec<u8>>> for RawTilt {
+    type Error = TiltErr;
+
+    fn try_from(manufacturer_data: &HashMap<u16, Vec<u8>>) -> Result<Self, Self::Error> {
+        bincode::DefaultOptions::new()
+            .with_fixint_encoding()
+            .allow_trailing_bytes()
+            .with_big_endian()
+            .deserialize::<RawTilt>(&ibeacon(manufacturer_data)?[..])
+            .map_err(|_| TiltErr::NotATilt)
+    }
+}
+
+impl TryFrom<&HashMap<u16, Vec<u8>>> for Tilt {
+    type Error = TiltErr;
+
+    fn try_from(manufacturer_data: &HashMap<u16, Vec<u8>>) -> Result<Self, Self::Error> {
+        let raw = RawTilt::try_from(manufacturer_data)?;
+
+        let temp = (raw.major as f32 - 32.0) / 1.8;
+        let gravity = (raw.minor as f32) / 1000.0;
+
+        if !(0.0..100.0).contains(&temp) {
+            return Err(TiltErr::BadValue);
+        }
+
+        Ok(Tilt {
+            name: tilt_name(raw.name)?,
+            temp,
+            gravity,
+        })
+    }
+}
 
 fn tilt_uuids() -> HashMap<Uuid, String> {
     "a495bb10c5b14b44b5121370f02d74de,Red
@@ -30,32 +81,15 @@ a495bb80c5b14b44b5121370f02d74de,Pink"
             hm
         })
 }
-fn tilt_name(data: &[u8]) -> Result<String, NotATilt> {
+fn tilt_name(data: uuid::Bytes) -> Result<String, TiltErr> {
     Ok(tilt_uuids()
-        .get(&Uuid::from_bytes(data.try_into().expect("len: 16")))
-        .ok_or(NotATilt)?
+        .get(&Uuid::from_bytes(data))
+        .ok_or(TiltErr::ButDucky)?
         .to_owned())
 }
 
-fn ibeacon(d: &HashMap<u16, Vec<u8>>) -> Result<Vec<u8>, NotATilt> {
-    d.get(&0x4c).map(|v| v.to_owned()).ok_or(NotATilt)
-}
-
-impl TryFrom<&HashMap<u16, Vec<u8>>> for Tilt {
-    type Error = NotATilt;
-
-    fn try_from(data: &HashMap<u16, Vec<u8>>) -> Result<Self, Self::Error> {
-        let v = ibeacon(data)?;
-        if v.len() != 23 {
-            return Err(NotATilt);
-        }
-        let read = |data: &[u8]| u16::from_be_bytes(data.try_into().expect("len: 2")) as f32;
-        Ok(Tilt {
-            name: tilt_name(&v[2..18])?,
-            temp: (read(&v[18..20]) - 32.0) / 1.8,
-            gravity: read(&v[20..22]) / 1000.0,
-        })
-    }
+fn ibeacon(d: &HashMap<u16, Vec<u8>>) -> Result<Vec<u8>, TiltErr> {
+    d.get(&76).map(|v| v.to_owned()).ok_or(TiltErr::NotIbeacon)
 }
 
 #[cfg(test)]
@@ -66,7 +100,7 @@ mod tests {
     fn values() {
         let tilt = Tilt::try_from(
             &[(
-                0x4c,
+                76,
                 vec![
                     2, 21, 164, 149, 187, 128, 197, 177, 75, 68, 181, 18, 19, 112, 240, 45, 116,
                     222, 0, 67, 4, 4, 34,
