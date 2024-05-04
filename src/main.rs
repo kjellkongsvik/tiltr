aa55c4717194562ff95c4496236c9b6527f79a02
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2024-05-04 09:58:38 +0200

    Calibrate gravity as single float arg to cli
    No need for clap to parse cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,35 @@
-use std::error::Error;
 mod tilt;
 use btleplug::api::{Central, Manager as _, Peripheral as _};
 use btleplug::platform::Manager;
 use std::convert::TryFrom;
-use tilt::Tilt;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let args = clap::App::new("tiltr")
-        .version(clap::crate_version!())
-        .arg(
-            clap::Arg::with_name("calibrate_g")
-                .short("c")
-                .default_value("0"),
-        )
-        .get_matches();
+async fn main() -> btleplug::Result<()> {
+    let mut calibrate_g: f32 = 0.0;
+    if let Some(g) = std::env::args().nth(1) {
+        calibrate_g = g.parse().unwrap();
+    }
 
-    let calibrate_g = clap::value_t!(args.value_of("calibrate_g"), f32)?;
-
-    println!("{}", scan_tilt(calibrate_g).await?);
-
-    Ok(())
-}
-
-async fn scan_tilt(calibrate_g: f32) -> Result<String, TiltError> {
     let adapter = Manager::new()
         .await?
         .adapters()
         .await?
         .into_iter()
         .next()
-        .ok_or(TiltError::MissingAdapter)?;
+        .unwrap();
     adapter.start_scan().await?;
 
     loop {
         for p in adapter.peripherals().await? {
             if let Some(k) = p.properties().await? {
-                if let Ok(mut t) = Tilt::try_from(&k.manufacturer_data) {
+                if let Ok(mut t) = tilt::Tilt::try_from(&k.manufacturer_data) {
                     t.gravity += calibrate_g;
                     adapter.stop_scan().await?;
-                    return Ok(serde_json::to_string(&t).unwrap());
+                    println!("{}", serde_json::to_string(&t).unwrap());
+
+                    return Ok(());
                 }
             }
         }
     }
-}
-
-#[derive(Debug, thiserror::Error)]
-enum TiltError {
-    #[error("BT")]
-    BT(#[from] btleplug::Error),
-    #[error("Missing adapter")]
-    MissingAdapter,
 }
